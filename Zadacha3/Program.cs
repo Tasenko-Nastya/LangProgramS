b37f8eddbd2e3e7066c2d9cbce08fd4baa9e4944
Anastasiya <Fankovskaya-asya@mail.ru>
2023-03-26 18:29:00 +0300

    Написала решение третей задачи

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число:");
+int num = int.Parse(Console.ReadLine());
+if(num % 2 ==0)
+{
+    Console.WriteLine("да");
+} 
+else
+{
+    Console.WriteLine("нет");
+}
