fdc817fdecd7cfd09dd395d6a22ee1e7b98a73f6
Anastasiya <Fankovskaya-asya@mail.ru>
2023-03-26 17:30:00 +0300

    Написала решение второй задачи

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите значения a:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите значения b:");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите значения c:");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (a>max)max = a;
+if (b>max)max = b;
+if (c>max)max = c;
+
+Console.WriteLine(max);
