748ffbed2d55aa544f14f6c2e9d246b667620bb4
Anastasiya <Fankovskaya-asya@mail.ru>
2023-03-26 16:31:26 +0300

    Написали решение первой задачи

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("a:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b:");
+int b = int.Parse(Console.ReadLine());
+if (a>b) 
+{
+    Console.WriteLine("a большее число, b меньшее число");
+}
+else
+{
+    Console.WriteLine("b большее число, a меньшее число");
+}
