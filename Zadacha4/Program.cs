9e461c00ac7af61c7c239049b013c3b4bf54e59d
Anastasiya <Fankovskaya-asya@mail.ru>
2023-03-26 20:27:38 +0300

    Написала решение 4 задачи

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+for (int i = 2; i < n+1; i++)
+{
+ if (i % 2 ==0)
+ {
+    Console.Write($"{i}");
+    
+ }
+}
+
