499ae3745ea77335a427076156ec9f06be5ed018
Anastasiya <Fankovskaya-asya@mail.ru>
2023-03-26 20:40:53 +0300

    Готовое решение задачи 1

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -3,6 +3,7 @@ Console.Write("a:");
 int a = int.Parse(Console.ReadLine());
 Console.Write("b:");
 int b = int.Parse(Console.ReadLine());
+
 if (a>b) 
 {
     Console.WriteLine($"{a} большее число, {b} меньшее число");
