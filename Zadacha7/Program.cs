73fcd44d79dc7dc7b25f1789d3b88e1e0c097251
Anastasiya <Fankovskaya-asya@mail.ru>
2023-03-28 18:22:04 +0300

    написала решение 7 задачи из дз 2 семинара

---
+++ Zadacha7/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите номер недели: ");
+int numberDay = int.Parse(Console.ReadLine());
+while(numberDay < 1 || numberDay > 7)
+{
+    Console.WriteLine("Введен неправильный номер");
+    Console.Write("Введите номер недели: ");
+    numberDay = int.Parse(Console.ReadLine());
+}
+if (numberDay > 5 && numberDay < 8)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Будний день");
+}
