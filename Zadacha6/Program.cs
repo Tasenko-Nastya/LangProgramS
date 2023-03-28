db8a1a21ec758154b5c6de6ec225900dc14713b0
Anastasiya <Fankovskaya-asya@mail.ru>
2023-03-28 17:34:30 +0300

    написала примерное решение 6 задачи из дз 2 семинара

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите значения a:");
+int a = int.Parse(Console.ReadLine());
+int n = 0;
+while (a>99)
+{
+    if (a<1000)
+    {
+        n = a % 10;
+        Console.WriteLine($"{n}");
+    }
+    else
+    {
+    a = a % 10;
+    }
+}    
+if (a<100)
+{
+    Console.WriteLine($"Третьей цифры нет");    
+}
