b200bbc8777bc7a787d30908deee657c5554670f
Anastasiya <Fankovskaya-asya@mail.ru>
2023-04-02 22:02:43 +0300

    Решила задачу 19 дз 3 семинара

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число:");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 10000 || num > 99999)
+{
+    Console.WriteLine("Введено неправильное число");
+    Console.Write("Введите пятизначное число:");
+    num = int.Parse(Console.ReadLine());
+}
+int N = num;
+int n = 0;
+int x = 0;
+
+while (num > 0)
+{
+        n = num % 10;
+        x = x * 10 +n;
+        num = num / 10;
+}
+Console.WriteLine(x);
+
+if(x == N)
+{
+    Console.Write($" Ваше число {N} является полидромом");
+}
+else
+{
+    Console.Write($" Ваше число {N} не является полидромом");
+}    
