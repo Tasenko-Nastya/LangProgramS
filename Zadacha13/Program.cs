1cb4165fcd39193e120c853ecfb8ce0fe81d94c5
Anastasiya <Fankovskaya-asya@mail.ru>
2023-04-08 19:02:01 +0300

    решила задачу 13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19], 6, 1, 33 -> [6, 1, 33]
+
+int[] GetArray(int size){
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 19);
+    }
+    return result;
+}
+int[] array = GetArray(5);
+Console.WriteLine($"[{String.Join(", ", array)}]");
