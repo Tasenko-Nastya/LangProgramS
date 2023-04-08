bbf5a2cf42417f8ab1b5578a34979a7445114a9b
Anastasiya <Fankovskaya-asya@mail.ru>
2023-04-08 18:14:10 +0300

    решила задачу 12

---
+++ Zadacha12/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+//452 -> 11, 82 -> 10, 9012 -> 12
+int Sum(int a){
+    int b = 0;
+    int c = 0;
+    int n = 0;
+    for (int i = 0; i <= a; i++)
+    {
+        b = a % 10;
+        a = a / 10;
+        n = n + b;
+    }
+    return n;
+}
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+ Console.WriteLine($"{Sum(a)}");
