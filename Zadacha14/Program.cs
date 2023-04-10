924b90ad5f15c09a0dccad8156ccdb14a9f7a7bc
Anastasiya <Fankovskaya-asya@mail.ru>
2023-04-10 17:36:25 +0300

    решила задачу 14

---
+++ Zadacha14/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+
+int[] GetArray(int size){
+    int[] res = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+       res[i] = new Random().Next(99, 1000); 
+    }
+    return res;
+}
+
+
+int Multiple(int[] array){
+    int count = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if(array[j] % 2 ==0)
+        count++;
+    }
+    return count;
+}
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = GetArray(size);
+Console.WriteLine($"[{String.Join(", ", array)}]");    
+ Console.Write($"Количество чётных чисел в массиве: {Multiple(array)}");
+
+
---
+++ Zadacha15/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19, [-4, -6, 89, 6] -> 0
+
+
+int[] GetArray(int size, int minV, int maxV){
+    int[] res = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+       res[i] = new Random().Next(minV, maxV); 
+    }
+    return res;
+}
+
+int sumnech(int[] Array);{
+    int sum = 0;
+    int j = 0;
+    
+}
