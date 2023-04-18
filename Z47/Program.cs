ceada24840358143729085b9da7cecfcc70d73a6
Anastasiya <Fankovskaya-asya@mail.ru>
2023-04-18 13:07:08 +0300

    решена задача 47

--- Z47/Program.cs
+++ Z47/Program.cs
@@ -3,3 +3,41 @@
 //0,5 7 -2 -0,2
 //1 -3,3 8 -9,9
 //8 7,8 -7,1 9
+
+double[,] GetArray(int m, int n, int min, int max)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            double num = new Random().NextDouble() * (max - min) + min;
+            result[i, j] = Math.Round(num, 1);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива n: ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = GetArray(m, n, -10, 10);
+PrintArray(array);
