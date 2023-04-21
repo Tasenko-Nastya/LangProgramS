fefad01c395514c6e99ef278ffb6e8ce6f64519b
Anastasiya <Fankovskaya-asya@mail.ru>
2023-04-21 10:45:52 +0300

    решила Z54

---
+++ Z54/Program.cs
@@ -0,0 +1,78 @@
+﻿//Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Sort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int z = 0; z < array.GetLength(1) - 1; z++)
+            {
+                if (array[i, z] < array[i, z + 1])
+                {
+                  int temp = array[i, z + 1];
+                  array[i, z + 1] = array[i, z];
+                  array[i, z] = temp;  
+                }
+                
+            }
+        
+        } 
+       
+    }
+    return array; 
+}
+
+
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива m: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов массива n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(m, n, 0, 100);
+
+PrintArray(array);
+Sort(array);
+Console.WriteLine();
+PrintArray(Sort(array));
+
+
