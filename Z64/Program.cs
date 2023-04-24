075d2bd9b701d19a77301631a4bc26d68a99d18d
Anastasiya <Fankovskaya-asya@mail.ru>
2023-04-24 16:31:30 +0300

    Z64

---
+++ Z64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+Console.Write("Введите положительное число n: ");
+int n = int.Parse(Console.ReadLine()!);
+int i = 1;
+WriteNumber( n,  i);
+
+
+
+void WriteNumber(int n, int i)
+{
+    if(i >  n) return;
+    WriteNumber( n,  i + 1);
+    Console.Write( i + ",");
+    
+    
+}
